e98daa6ebfebecd03c67d0488aea960d07d0c9d6
Paulo César <paulo.csa@outlook.com>
2022-05-09 17:28:29 -0300

    nova atividade

---
+++ Atividades JS - Senai/print-numeroPar.js
@@ -0,0 +1,16 @@
+//Faça um programa que receba um número de usuário e exiba este
+//número, apenas se for par.
+
+
+let capNumero = require("readline-sync")
+
+let numero = capNumero.question("Informe um numero aleatorio: ")
+
+let resultado = numero % 2
+
+if (resultado == 0){
+    console.log("O numero escolhido foi: "+ numero +" e é par." )
+    
+}else{
+    console.log("so numero par sera apresentado em tela !")
+}
